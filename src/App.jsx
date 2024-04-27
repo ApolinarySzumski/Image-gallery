e17de97d3a12d8280c98a5166db04a8cd0d79c3e
ApolinarySzumski <apolinary@poczta.onet.pl>
2024-04-27 21:36:19 +0200

    fixed function import

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { useEffect, useState } from "react";
 import css from "./App.module.css";
+import { callToApi } from "./Functions/callToApi";
 import { Button } from "./components/Button/Button";
 import { ImageGallery } from "./components/ImageGallery/ImageGallery";
 import { ImageGalleryItem } from "./components/ImageGalleryItem/ImageGalleryItem";
 import { Loader } from "./components/Loader/Loader";
 import { Modal } from "./components/Modal/Modal";
 import { Searchbar } from "./components/Searchbar/Searchbar";
-import { callToApi } from "./functions/callToApi";
 import { useToggle } from "./hooks/useToggle";
 
 const App = () => {
