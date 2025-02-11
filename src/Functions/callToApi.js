c2b4e3655b7c2be6bb52e568c8313639263cf613
ApolinarySzumski <apolinary@poczta.onet.pl>
2025-02-11 13:48:42 +0100

    fix: wrong import in app.jsx

--- src/Functions/callToApi.js
+++ src/Functions/callToApi.js
@@ -1,6 +1,6 @@
 // import md5 from 'md5';
 
-export const callToApi = async (query, page) => {
+const callToApi = async (query, page) => {
   const API_KEY_BEFORE_HIDE = "41545287-03d64db3978578b8466972b4a";
   // const API_KEY = md5(API_KEY_BEFORE_HIDE);
   const endpoint = `https://pixabay.com/api/?key=${API_KEY_BEFORE_HIDE}&q=${query}&page=${page}&image_type=photo&orientation=horizontal&per_page=12`;
@@ -8,3 +8,5 @@ export const callToApi = async (query, page) => {
   const extractedResp = await resp.json();
   return extractedResp;
 };
+
+export default callToApi;
